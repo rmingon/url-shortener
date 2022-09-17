4d2865fe369edbf5ec952952be9fefc205c312c6
rmingon <ronanmingon69550@gmail.com>
2022-09-17 09:32:19 +0200

    features

---
+++ front/components/Features.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex w-full h-56 bg-white -mt-24 z-10">
+    <div class="flex flex-row mx-16 w-full rounded-lg mt-24 justify-around shadow-lg h-32 cursor-pointer">
+        <div class="hover:bg-slate-200 hover:border-b-4 hover:border-blue-500 basis-1/5 pt-3 duration-200">
+          <div class="text-center text-2xl montserrat-extra-bold mt-4">
+            1M+
+          </div>
+          <div class="text-center mt-2">
+            Links created
+          </div>
+        </div>
+        <div class="hover:bg-slate-200 hover:border-b-4 hover:border-blue-500 basis-1/5 pt-3 duration-200">
+          <div class="text-center text-2xl montserrat-extra-bold mt-4">
+            18M+
+          </div>
+          <div class="text-center mt-2">
+            Redirects links
+          </div>
+        </div>
+        <div class="hover:bg-slate-200 hover:border-b-4 hover:border-blue-500 basis-1/5 pt-3 duration-200">
+          <div class="text-center text-2xl montserrat-extra-bold mt-4">
+            5M+
+          </div>
+          <div class="text-center mt-2">
+            Accounts
+          </div>
+        </div>
+        <div class="hover:bg-slate-200 hover:border-b-4 hover:border-blue-500 basis-1/5 pt-3 duration-200">
+          <div class="text-center text-2xl montserrat-extra-bold mt-4">
+            3,652
+          </div>
+          <div class="text-center mt-2">
+            Redirects day
+          </div>
+        </div>
+        <div class="hover:bg-slate-200 hover:border-b-4 hover:border-blue-500 basis-1/5 pt-3 duration-200">
+          <div class="text-center text-2xl montserrat-extra-bold mt-4">
+            6,543
+          </div>
+          <div class="text-center mt-2">
+            Backlinks
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
