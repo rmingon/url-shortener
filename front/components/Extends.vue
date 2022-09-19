d29bd85c9c9f3054e27f4f7309a7499ed0d76b76
rmingon <ronanmingon69550@gmail.com>
2022-09-19 06:56:50 +0200

    add extends

---
+++ front/components/Extends.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="h-40 px-14 bg-white w-full flex flex-col">
+    <div class="flex flex-row w-full justify-between items-center my-12">  
+      <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
+        <div class="flex items-center cursor-pointer mr-2">
+          <svg width="60" height="60" viewBox="0 0 24 24"><path fill="currentColor" d="m12 20l3.46-6h-.01c.34-.6.55-1.27.55-2c0-1.2-.54-2.27-1.38-3h4.79c.38.93.59 1.94.59 3a8 8 0 0 1-8 8m-8-8c0-1.46.39-2.82 1.07-4l3.47 6h.01c.69 1.19 1.95 2 3.45 2c.45 0 .88-.09 1.29-.23l-2.4 4.14C7 19.37 4 16.04 4 12m11 0a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3m-3-8a7.98 7.98 0 0 1 6.92 4H12c-1.94 0-3.55 1.38-3.92 3.21L5.7 7.08A7.981 7.981 0 0 1 12 4m0-2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"/></svg>  
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="MontserratBold">
+            Chrome
+          </div>
+          <div>
+            Best plug-in
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="border-2 rounded-full cursor-pointer -rotate-90">
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"/></svg>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
+        <div class="flex items-center cursor-pointer mr-2">
+          <svg width="60" height="60" viewBox="0 0 24 24"><path fill="currentColor" d="M6 15a2 2 0 0 1-2 2a2 2 0 0 1-2-2a2 2 0 0 1 2-2h2v2m1 0a2 2 0 0 1 2-2a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2a2 2 0 0 1-2-2v-5m2-8a2 2 0 0 1-2-2a2 2 0 0 1 2-2a2 2 0 0 1 2 2v2H9m0 1a2 2 0 0 1 2 2a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2a2 2 0 0 1 2-2h5m8 2a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2h-2v-2m-1 0a2 2 0 0 1-2 2a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2a2 2 0 0 1 2 2v5m-2 8a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2v-2h2m0-1a2 2 0 0 1-2-2a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2a2 2 0 0 1-2 2h-5Z"/></svg>
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="MontserratBold">
+            Slack
+          </div>
+          <div>
+            Best plug-in
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="border-2 rounded-full cursor-pointer -rotate-90">
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"/></svg>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
+        <div class="flex items-center cursor-pointer mr-2">
+          <svg class="stokes-sky-700" width="60" height="60" viewBox="0 0 24 24"><path d="M3.42 12c0-1.24.27-2.42.74-3.5l4.1 11.22A8.563 8.563 0 0 1 3.42 12m14.37-.43c0 .73-.29 1.58-.65 2.77l-.86 2.86l-3.1-9.2l.98-.1c.47-.06.41-.74-.05-.71c0 0-1.39.11-2.29.11l-2.26-.11c-.46-.03-.51.68-.06.71l.91.1l1.34 3.64l-1.88 5.63L6.74 8l.99-.1c.46-.06.4-.74-.06-.71c0 0-1.39.11-2.29.11l-.55-.01C6.37 4.96 9 3.42 12 3.42c2.23 0 4.27.86 5.79 2.25h-.11c-.84 0-1.44.73-1.44 1.52c0 .71.41 1.31.84 2.01c.33.57.71 1.3.71 2.37m-5.64 1.18l2.64 7.22l.06.12c-.89.32-1.85.49-2.85.49c-.84 0-1.65-.12-2.42-.35l2.57-7.48m7.38-4.87A8.548 8.548 0 0 1 20.58 12c0 3.16-1.72 5.93-4.27 7.41l2.62-7.57c.49-1.22.66-2.2.66-3.07l-.06-.89M12 2a10 10 0 0 1 10 10a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2m0 19.54c5.26 0 9.54-4.28 9.54-9.54c0-5.26-4.28-9.54-9.54-9.54c-5.26 0-9.54 4.28-9.54 9.54c0 5.26 4.28 9.54 9.54 9.54Z"/></svg>
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="MontserratBold">
+            Wordpress
+          </div>
+          <div>
+            Best plug-in
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="border-2 rounded-full cursor-pointer -rotate-90">
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"/></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row w-full justify-between items-center my-12">  
+      <div class="basis-1/6"></div>
+      <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
+        <div class="flex items-center cursor-pointer mr-2">
+          <svg width="60" height="60" viewBox="0 0 24 24"><path fill="currentColor" d="m12 2.5l8.84 3.15l-1.34 11.7L12 21.5l-7.5-4.15l-1.34-11.7L12 2.5m0 2.1L6.47 17h2.06l1.11-2.78h4.7L15.45 17h2.05L12 4.6m1.62 7.9h-3.23L12 8.63l1.62 3.87Z"/></svg>
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="MontserratBold">
+            Angular
+          </div>
+          <div>
+            Best plug-in
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="border-2 rounded-full cursor-pointer -rotate-90">
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"/></svg>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
+        <div class="flex items-center cursor-pointer mr-2">
+          <svg width="60" height="60" viewBox="0 0 24 24"><path fill="currentColor" d="M19.14 7.5A2.86 2.86 0 0 1 22 10.36v3.78A2.86 2.86 0 0 1 19.14 17H12c0 .39.32.96.71.96H17v1.68a2.86 2.86 0 0 1-2.86 2.86H9.86A2.86 2.86 0 0 1 7 19.64v-3.75a2.85 2.85 0 0 1 2.86-2.85h5.25a2.85 2.85 0 0 0 2.85-2.86V7.5h1.18m-4.28 11.79c-.4 0-.72.3-.72.89c0 .59.32.71.72.71a.71.71 0 0 0 .71-.71c0-.59-.32-.89-.71-.89m-10-1.79A2.86 2.86 0 0 1 2 14.64v-3.78A2.86 2.86 0 0 1 4.86 8H12c0-.39-.32-.96-.71-.96H7V5.36A2.86 2.86 0 0 1 9.86 2.5h4.28A2.86 2.86 0 0 1 17 5.36v3.75a2.85 2.85 0 0 1-2.86 2.85H8.89a2.85 2.85 0 0 0-2.85 2.86v2.68H4.86M9.14 5.71c.4 0 .72-.3.72-.89c0-.59-.32-.71-.72-.71c-.39 0-.71.12-.71.71s.32.89.71.89Z"/></svg>
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="MontserratBold">
+            Python
+          </div>
+          <div>
+            Just why not
+          </div>
+        </div>
+        <div class="flex flex-col">
+          <div class="border-2 rounded-full cursor-pointer -rotate-90">
+            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6l1.41-1.41z"/></svg>
+          </div>
+        </div>
+      </div>
+      <div class="basis-1/6"></div>
+    </div>
+  </div>
+</template>
