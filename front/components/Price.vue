0885aee033f4c726c9604d72ae0be4e2cec839e1
rmingon <ronanmingon69550@gmail.com>
2022-09-20 06:48:59 +0200

    price size

--- front/components/Price.vue
+++ front/components/Price.vue
@@ -1,3 +1,7 @@
+<script setup>
+  
+</script>
+
 <template>
   <div class="px-32 py-6">
     <div class="text-5xl text-sky-200 text-center MontserratBold">
@@ -6,7 +10,7 @@
     <div class="flex flex-row justify-center items-center mt-10">
       <div class="basis-1/3 bg-slate-50 rounded-l h-72">
         <div class="text-center text-xl MontserratClassic my-3">Free</div>
-        <div class="border-b mx-4"></div>
+        <div class="border-b border-sky-800 mx-8"></div>
         <div class="flex flex-row justify-center items-center m-2 bg-green-400 duration-200 hover:bg-green-500 cursor-pointer rounded-sm py-2">
           <div class="mr-2">
             <svg class="stroke-white" width="32" height="32" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M3.5 9.5c0-1.3 3.134-3 7-3s7 1.7 7 3v3c0 1.3-3.134 3-7 3s-7-1.7-7-3v-3z"/><path d="M10.5 12.484c3.866 0 7-1.606 7-2.986c0-1.381-3.134-2.998-7-2.998s-7 1.617-7 2.998c0 1.38 3.134 2.986 7 2.986z"/></g></svg>
@@ -16,12 +20,12 @@
       </div>
       <div class="basis-1/3 bg-white rounded h-96 shadow-lg flex flex-col">
         <div class="text-center text-xl MontserratClassic my-3">Paid</div>
-        <div class="border-b mx-4"></div>
+        <div class="border-b border-sky-800 mx-8"></div>
         <div>Unlimited links</div>
         <div>Unlimited barcodes</div>
         <div>Analystics</div>
         <div>Analystics</div>
-        <div class="flex flex-row justify-center items-center m-2 bg-green-400 duration-200 hover:bg-green-500 cursor-pointer rounded-sm py-2">
+        <div class="flex flex-row justify-center items-center m-2 bg-green-400 duration-200 hover:bg-green-500 cursor-pointer rounded-sm py-2 active:shadow-inner">
           <div class="mr-2">
             <svg class="stroke-white" width="32" height="32" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M18.5 11.5v3c0 1.3-3.134 3-7 3s-7-1.7-7-3V12"/><path d="M4.794 12.259c.865 1.148 3.54 2.225 6.706 2.225c3.866 0 7-1.606 7-2.986c0-.775-.987-1.624-2.536-2.22"/><path d="M15.5 6.5v3c0 1.3-3.134 3-7 3s-7-1.7-7-3v-3"/><path d="M8.5 9.484c3.866 0 7-1.606 7-2.986c0-1.381-3.134-2.998-7-2.998s-7 1.617-7 2.998c0 1.38 3.134 2.986 7 2.986z"/></g></svg>
           </div>
@@ -30,7 +34,7 @@
       </div>
       <div class="basis-1/3 bg-slate-50 rounded-r h-72">
         <div class="text-center text-xl MontserratClassic my-3">Register</div>
-        <div class="border-b mx-4"></div>
+        <div class="border-b border-sky-800 mx-8"></div>
         <div class="flex flex-row justify-center items-center m-2 bg-green-400 duration-200 hover:bg-green-500 cursor-pointer rounded-sm py-2">
           <div class="mr-2">
             <svg class="stroke-white" width="32" height="32" viewBox="0 0 21 21"><g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M3.5 9.5c0-1.3 3.134-3 7-3s7 1.7 7 3v3c0 1.3-3.134 3-7 3s-7-1.7-7-3v-3z"/><path d="M10.5 12.484c3.866 0 7-1.606 7-2.986c0-1.381-3.134-2.998-7-2.998s-7 1.617-7 2.998c0 1.38 3.134 2.986 7 2.986z"/></g></svg>
