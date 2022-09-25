1111040477498e86106912725611648896422359
rmingon <ronanmingon69550@gmail.com>
2022-09-25 09:39:32 +0200

    add height

--- components/Extends.vue
+++ components/Extends.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-40 px-14 bg-white w-full flex flex-col">
-    <div class="flex flex-row w-full justify-between items-center my-12">  
+  <div class="h-64 px-14 bg-white w-full flex flex-col">
+    <div class="flex flex-row w-full justify-between items-center mt-2">  
       <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
         <div class="flex items-center cursor-pointer mr-2">
           <svg width="60" height="60" viewBox="0 0 24 24"><path fill="currentColor" d="m12 20l3.46-6h-.01c.34-.6.55-1.27.55-2c0-1.2-.54-2.27-1.38-3h4.79c.38.93.59 1.94.59 3a8 8 0 0 1-8 8m-8-8c0-1.46.39-2.82 1.07-4l3.47 6h.01c.69 1.19 1.95 2 3.45 2c.45 0 .88-.09 1.29-.23l-2.4 4.14C7 19.37 4 16.04 4 12m11 0a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3m-3-8a7.98 7.98 0 0 1 6.92 4H12c-1.94 0-3.55 1.38-3.92 3.21L5.7 7.08A7.981 7.981 0 0 1 12 4m0-2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"/></svg>  
@@ -56,7 +56,7 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-row w-full justify-between items-center my-12">  
+    <div class="flex flex-row w-full justify-between items-center mt-3">  
       <div class="basis-1/6"></div>
       <div class="flex flex-row items-center basis-1/3 mx-4 my-2 rounded-lg shadow p-4 hover:shadow-lg hover:border-t-4 hover:border-sky-600 duration-200">
         <div class="flex items-center cursor-pointer mr-2">
